93dd610bbe2989fc0b773fb6a43fb4b94ae6dc1c
Dimitar Zhechev <dimityr.jechev@gmail.com>
2018-06-26 19:17:05 +0300

    Initial commit

---
+++ Linear-Data-Structures/ArrayList/Lists/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        var demoArr = new ArrayList<int>();
+        demoArr.Add(3);
+        demoArr.Add(4);
+        demoArr.Add(5);
+        demoArr.Add(6);
+        demoArr.RemoveAt(2);
+    }
+}
---
+++ Linear-Data-Structures/Exercises/LinearDataStructuresExercies/01SumAndAverage/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01SumAndAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ');
+            int sum = 0;
+            int avg = 0;
+            List<int> listWithNumbers = new List<int>();
+
+            foreach (var number in input)
+            {
+                listWithNumbers.Add(int.Parse(number));
+            }
+
+            foreach (var number1 in listWithNumbers)
+            {
+                sum += number1;
+            }
+            avg = sum / listWithNumbers.Count();
+        }
+    }
+}
